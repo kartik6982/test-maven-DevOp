c9ec245fb90f6413671bc421032941f0fbc3ea2f
kartik6982 <140691462+kartik6982@users.noreply.github.com>
2025-08-04 21:52:47 +0530

    Initial DevOps test project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🚀 DevOps Test Project Started!");
+        System.out.println("Running Java app in IntelliJ with Maven.");
+    }
+}
